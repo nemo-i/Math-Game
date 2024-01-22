07a7fe5220b31eea1283a8ee503eebb365e9c8cb
nemo-i <tanton509@outlook.com>
2024-01-22 06:54:53 +0200

    Some Polsing Of The Game

--- Math Game.cpp
+++ Math Game.cpp
@@ -48,18 +48,13 @@ short ReadOnlyNumber(string msg) {
     return number;
 }
 
-bool ReadNumberInRange(short from, short to,short number) {
-   
+bool ReadNumberInRange(short from, short to,short number) { 
   return number >= from && number <= to;
 }
-
-
 
 short ReadQuestionsCount(){
  return ReadOnlyNumber("How Many Question Do You Want To Answer ? ");
 }
-
-
 
 enGameLevel ReadGameLevel() {
     short level;
@@ -99,20 +94,221 @@ string GetGameLevelName(enGameLevel gameLevel) {
         break;
     }
 }
+struct sRoundValues {
+    short numerator = 0;
+    short denominator = 0;
+    char operationType = '+';
+    short userAnswer = 0;
+    short correctAnswer = 0;
+    bool isUserAnswerCorrectly;
+};
+
+
+string PassOrFail(sGameData gameData) {
+    if (gameData.IsPlayerPassed) {
+        return "Pass";
+    }
+    else
+    {
+        return "Fail";
+    }
+}
+string SadFace() {
+    return ";-(";
+}
+
+string HappyFace() {
+    return ":-)";
+}
+
+string SadOrHappyFace(sGameData gameData) {
+    if (gameData.IsPlayerPassed) {
+        return HappyFace();
+    }
+    else
+    {
+        return SadFace();
+    }
+}
+
+void PrintRoundQuestionHeader(short questionNumber,short totalQuestions ) {
+    cout << "Question [" << questionNumber << "/" << totalQuestions << "]" << endl;
+}
+
 struct sGameData {
     short QuestionCount = 0;
     enGameLevel GameLevel = enGameLevel::Easy;
     enQuestionType QuestionType = enQuestionType::Add;
+    short RightAnswersCount = 0;
+    short WrongAnswersCount = 0;
+    bool IsPlayerPassed = true;
+    struct sRoundValues& roundValues;
 };
+
+short ReadUserAnswer() {
+    short userAnswer;
+    cin >> userAnswer;
+    return userAnswer;
+}
+char OperationType(enQuestionType questionType) {
+    switch (questionType)
+    {
+    case Add:
+        return '+';
+        break;
+    case Sub:
+        return '-';
+        break;
+    case Mul:
+        return '*';
+        break;
+    case Div:
+        return '/';
+        break;
+    }
+};
+
+short GetRandNumberInRange(short from, short to) {
+    return rand() % (to - from + 1) + from;
+}
+
+void PrintQuestionExpresion(sGameData gameData) {
+    cout << gameData.roundValues.numerator << endl;
+    cout << gameData.roundValues.denominator << " " << OperationType(gameData.QuestionType) << endl;
+    cout << "---------------" << endl;
+}
+
+
+
+void PrintRightAnswer(sRoundValues roundValues) {
+    cout << "The Answer Is : " << roundValues.correctAnswer<<endl;
+}
+
+void VerticalSpacing(short number) {
+    for (short i = 0; i < number; i++)
+    {
+        cout << "\n";
+    }
+}
+
+
+void BackgroundColor(bool isCorrectlyAnswerThisQuestion) {
+    switch (isCorrectlyAnswerThisQuestion)
+    {case true:
+        system("color 2F");
+        break;
+    default:
+        system("color 0F");
+        break;
+    }
+}
+
+void WrongAnswerSoundEffect(bool isCorrectlyAnswerThisQuestion){
+    if (!isCorrectlyAnswerThisQuestion) {
+        cout << "\a";
+}
+}
+void ShowGameEffects(sRoundValues roundValues) {
+    BackgroundColor(roundValues.isUserAnswerCorrectly);
+    WrongAnswerSoundEffect(roundValues.isUserAnswerCorrectly);
+}
+
+
+void PrintRoundResults(sRoundValues roundValues) {
+    if (roundValues.isUserAnswerCorrectly) {
+        cout << "Right Answer " << HappyFace()<<endl;
+    }
+    else
+    {
+        cout << "Wrong Answer " << SadFace()<<endl;
+        PrintRightAnswer(roundValues);
+        
+    }
+    ShowGameEffects(roundValues);
+    VerticalSpacing(2);
+}
+
+
+
+
+void ReadGameData(sGameData &gameData) {
+    gameData.QuestionCount = ReadQuestionsCount();
+    gameData.GameLevel = ReadGameLevel();
+    gameData.QuestionType = ReadQuestionType();
+}
+
+
+
+
+
+
+void PlayGameRounds(sGameData gameData) {
+    sRoundValues roundValue;
+
+    for (short Question = 1; Question <= gameData.QuestionCount; Question++)
+    {
+        PrintRoundQuestionHeader(Question, gameData.QuestionCount);
+        VerticalSpacing(1);
+        PrintQuestionExpresion(gameData);
+        ReadUserAnswer();
+        PrintRoundResults(roundValue);
+    }
+}
+void PrintFinalResultHeader(string passedOrFail, string face) {
+    cout << "-------------------------------------" << endl;
+    cout << " Final Result Is " << passedOrFail << face << endl;
+    cout << "-------------------------------------" << endl;
+}
+void PrintFinalResultPanal(sGameData gameData) {
+    cout << "Number Of Questions : " << gameData.QuestionCount << endl;
+    cout << "Question Level      : " << GetGameLevelName(gameData.GameLevel) << endl;
+    cout << "Operation Type      : " << GetQuestionTypeName(gameData.QuestionType) << endl;
+    cout << "Number Of Right Answers: " << gameData.RightAnswersCount << endl;
+    cout << "Number Of Wrong Answers: " << gameData.WrongAnswersCount << endl;
+    cout << "-------------------------------------" << endl;
+}
+
+void PrintFinalResult(sGameData gameData) {
+    PrintFinalResultHeader(PassOrFail(gameData), SadOrHappyFace(gameData));
+    VerticalSpacing(1);
+    PrintFinalResultPanal(gameData);
+}
+void StartGame() {
+    sGameData gameData;
+    ReadGameData(gameData);
+    PlayGameRounds(gameData);
+    PrintFinalResult(gameData);
+}
+
+void AskUserIfHeWantToPlayAgain() {
+    cout << "Do You Want To Play Again? Y/N? " << endl;
+}
+
+
+
+
+void GameLoop() {
+    char again;
+    do
+    {
+        StartGame();
+        
+        AskUserIfHeWantToPlayAgain();
+        cin >> again;
+        
+    } while (again =='Y'||again == 'y');
+
+}
+
 void Start() {
     ReadQuestionsCount();
     ReadGameLevel();
     ReadQuestionType();
+   
 }
 int main()
 {
-    Start();
-    std::cout << "Hello World!\n";
+    GameLoop();
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
